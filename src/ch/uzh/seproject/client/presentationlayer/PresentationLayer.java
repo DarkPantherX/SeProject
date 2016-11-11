8503a9118fe5ba312a9dfd300bf00ce66777ecb8
tobinfelder <tobin.f@hotmail.com>
2016-11-11 13:47:29 +0100

    GUI
    GUI Sprint 1 Done, no data connection yet (kei plan was all da unitCache
    züg esch)

--- src/ch/uzh/seproject/client/presentationlayer/PresentationLayer.java
+++ src/ch/uzh/seproject/client/presentationlayer/PresentationLayer.java
@@ -1,43 +1,57 @@
 package ch.uzh.seproject.client.presentationlayer;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.JavaScriptObject;
 import com.google.gwt.dom.client.Document;
 import com.google.gwt.dom.client.Style.Display;
 import com.google.gwt.user.client.DOM;
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.FormPanel;
+import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import ch.uzh.seproject.client.businesslogiclayer.BusinessLogicLayer;
 
 import ch.uzh.seproject.client.businesslogiclayer.BusinessLogicLayer;
 
 /**
  * This is the main inteface for the Presentation-Layer.
  */
-public class PresentationLayer implements EntryPoint{
+public class PresentationLayer implements EntryPoint {
 	// business-logic-layer
 	private BusinessLogicLayer bll = new BusinessLogicLayer();
 	private Button tableButton = Button.wrap(Document.get().getElementById("tableButton"));
 	private Button worldmapButton = Button.wrap(Document.get().getElementById("worldmapButton"));
+	//private Button uploadButton = Button.wrap(Document.get().getElementById("UploadButton"));
+	
 	/**
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
-		this.tableButton.addClickHandler(new ClickHandler() {
-			public void onClick(ClickEvent event){
-				DOM.getElementById("table").getStyle().setDisplay(Display.BLOCK);
-				DOM.getElementById("worldmap").getStyle().setDisplay(Display.NONE);
-				DOM.getElementById("tableButton").addClassName("active");
-				DOM.getElementById("worldmapButton").removeClassName("active");
-			}
-		});
 		this.worldmapButton.addClickHandler(new ClickHandler() {
 			public void onClick(ClickEvent event){
-				DOM.getElementById("table").getStyle().setDisplay(Display.NONE);
 				DOM.getElementById("worldmap").getStyle().setDisplay(Display.BLOCK);
+				DOM.getElementById("table").getStyle().setDisplay(Display.NONE);
 				DOM.getElementById("worldmapButton").addClassName("active");
 				DOM.getElementById("tableButton").removeClassName("active");
 			}
+		});
+		this.tableButton.addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event){
+				DOM.getElementById("worldmap").getStyle().setDisplay(Display.NONE);
+				DOM.getElementById("table").getStyle().setDisplay(Display.BLOCK);
+				DOM.getElementById("tableButton").addClassName("active");
+				DOM.getElementById("worldmapButton").removeClassName("active");
+			}
 		});    
+		/**this.uploadButton.addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event){
+			}
+		});*/
+		
+		
+		//Create Map
+		
 	}
 	
 	
@@ -80,3 +94,4 @@ public class PresentationLayer implements EntryPoint{
 	}
 
 }
+
