56edf2288d38beba11600c54e36423dfef5321c5
tobinfelder <tobin.f@hotmail.com>
2016-11-28 12:37:38 +0100

    .

--- src/ch/uzh/seproject/client/presentationlayer/PresentationLayer.java
+++ src/ch/uzh/seproject/client/presentationlayer/PresentationLayer.java
@@ -8,34 +8,42 @@ import java.util.logging.Logger;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.JavaScriptObject;
 import com.google.gwt.dom.client.Document;
+import com.google.gwt.dom.client.Element;
 import com.google.gwt.dom.client.Style.Display;
+import com.google.gwt.dom.client.TableElement;
 import com.google.gwt.user.client.DOM;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.FormPanel;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.HTMLPanel;
+import com.google.gwt.user.client.ui.HTMLTable;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.i18n.client.DateTimeFormat;
+import com.google.gwt.thirdparty.javascript.rhino.head.ast.Label;
 
 import ch.uzh.seproject.client.businesslogiclayer.BusinessLogicLayer;
 import ch.uzh.seproject.client.dataaccesslayer.WeatherRecord;
-import ch.uzh.seproject.client.businesslogiclayer.BusinessLogicLayer;
-
+import ch.uzh.seproject.client.dataaccesslayer.DataAccessLayer;
 /**
  * This is the main inteface for the Presentation-Layer.
  */
 public class PresentationLayer implements EntryPoint {
 	// business-logic-layer
 	private BusinessLogicLayer bll = new BusinessLogicLayer();
+	private DataAccessLayer dal = new DataAccessLayer();
 	private Button tableButton = Button.wrap(Document.get().getElementById("tableButton"));
 	private Button worldmapButton = Button.wrap(Document.get().getElementById("worldmapButton"));
-	//private Button uploadButton = Button.wrap(Document.get().getElementById("UploadButton"));
 	
 	/**
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
+		
+		
 		this.worldmapButton.addClickHandler(new ClickHandler() {
 			public void onClick(ClickEvent event){
 				DOM.getElementById("worldmap").getStyle().setDisplay(Display.BLOCK);
@@ -54,22 +62,34 @@ public class PresentationLayer implements EntryPoint {
 				DOM.getElementById("worldmapButton").removeClassName("active");
 			}
 		});    
-		/**this.uploadButton.addClickHandler(new ClickHandler() {
-			public void onClick(ClickEvent event){
+		
+		/*
+		dal.getWeatherData(new AsyncCallback<List<WeatherRecord>>() {
+			@Override 
+			public void onFailure(Throwable caught) { }
+		
+			@Override 
+			public void onSuccess(List<WeatherRecord> result) { 
+				// result is the list you want 
+				// this function is executed when the datais ready (asynchronous) 
+				
+				// create logger to print to webbrowser (developer mode)
+				// print every result from 2012 to 2013
+				for (WeatherRecord tmp : result) {
+					HTML html = new HTML("<tr><td></td><td>" + tmp.getCity() + "</td><td></td><td></td><td></td><td></td><td></td></tr>");
+					RootPanel.get("dataTable").add(html);
 			}
-		});*/
-		
-		
+		}});
 		/*
 		 * Example to get weather data from 2012 to 2013
 		 */
+		
 		Date from = DateTimeFormat.getFormat("yyyy-MM-dd HH:mm:ss").parse("2012-01-01 00:00:00");
 		Date to = DateTimeFormat.getFormat("yyyy-MM-dd HH:mm:ss").parse("2013-01-01 00:00:00");
 		
 		bll.getWeatherData(from, to, new AsyncCallback<List<WeatherRecord>>() {
 			@Override 
 			public void onFailure(Throwable caught) { }
-		
 			@Override 
 			public void onSuccess(List<WeatherRecord> result) { 
 				// result is the list you want 
